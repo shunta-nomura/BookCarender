c63b946a473b00d399f6bf1423cf1b5a36827374
shunta-nomura <58507009+shunta-nomura@users.noreply.github.com>
2022-08-05 01:13:46 +0900

    初期作成

---
+++ BookCarender/BookCarender/BookCarenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookCarenderApp.swift
+//  BookCarender
+//
+//  Created by 野村俊太 on 2022/08/02.
+//
+
+import SwiftUI
+
+@main
+struct BookCarenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookCarender/BookCarender/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookCarender
+//
+//  Created by 野村俊太 on 2022/08/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
