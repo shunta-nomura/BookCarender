2e51ece41069dd057ca3c50ce9c4702d89aaf40d
野村俊太 <ns_is_a_pen@icloud.com>
2022-08-18 02:58:40 +0900

    #1
    pod 修正 アプリ起動時の処理作成

--- BookCarender/BookCarender/BookCarenderApp.swift
+++ BookCarender/BookCarender/BookCarenderApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct BookCarenderApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        print("起動処理完了")
+
+        return true
     }
 }
